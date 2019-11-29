e13c7d26f5ead799a4b24202d108e18deab8518a
Nishant Saxena <44865935+xphoenix1010@users.noreply.github.com>
2019-11-29 18:10:54 +0530

    Initial commit

---
+++ Chapter-3/12.cpp
@@ -0,0 +1,54 @@
+/*
+Create a four-function calculator for fractions.
+*/
+
+//author @Nishant
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	char ch;
+	do{
+		int a, b, c, d, num, den;
+		char temp, option;
+		cout << "Enter first fraction: ";
+		cin >> a >> temp >> b;
+		cout << "Operator: ";
+		cin >> option;
+		cout << "Enter second fraction: ";
+		cin >> c >> temp >> d;
+
+		switch(option){
+			case '+':
+				num = a * d + b * c;
+				den = b * d;
+				cout << "Sum is: " << num << "/" << den << endl;
+				break;
+			case '-':
+				num = a * d - b * c;
+				den = b * d;
+				cout << "Subtraction is: " << num << "/" << den << endl;
+				break;
+			case '*':
+				num = a * c;
+				den = b * d;
+				cout << "Multiplication is: " << num << "/" << den << endl;
+				break;
+			case '/':
+				num = a * d;
+				den = b * c;
+				cout << "Division is: " << num << "/" << den << endl;
+				break;
+			default:
+				cout << "Invalid option";
+				break;
+		
+		}
+		cout << "Do you want to continue (y/n): ";
+		cin >> ch;
+	}while(ch == 'y');
+	
+	return 0;
+}
